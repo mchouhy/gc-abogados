835fbcfc8471b57318b8103a476c37d54a360194
mchouhy <marianochouhy@gmail.com>
2026-05-27 16:41:10 +0200

    WIP: Mobile version & new version

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// prettier.config.js
+
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  tailwindStylesheet: "./src/styles/global.css",
+};
